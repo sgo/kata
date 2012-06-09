fff8d3031a7934b6f11eca18be84a3f57052daff
sgo <sgo@thinkerit.be>
2012-06-09 16:54:14 +0200

    PrimeFactors kata solved

---
+++ primefactors/src/test/groovy/kata/PrimeFactors.groovy
@@ -0,0 +1,53 @@
+package kata
+
+class PrimeFactors {
+    static def of(def n) {
+        new PrimeFactorsCalculator(number: n).factors
+    }
+
+    private static class PrimeFactorsCalculator {
+        def factors = []
+        def factor = 1
+        BigInteger number
+
+        def getFactors() {
+            while (notAllFactorsFound()) {
+                nextGuess()
+                while (divisible) {
+                    addFactor()
+                    divideByFactor()
+                }
+                if (onlySelfLeft) nextFactorIsSelf()
+            }
+            return factors
+        }
+
+        private boolean notAllFactorsFound() {
+            number > 1
+        }
+
+        private void nextGuess() {
+            factor++
+        }
+
+        private boolean isDivisible() {
+            number % factor == 0
+        }
+
+        private void addFactor() {
+            factors << factor
+        }
+
+        private void divideByFactor() {
+            number /= factor
+        }
+
+        private boolean isOnlySelfLeft() {
+            factor > Math.sqrt(number)
+        }
+
+        private void nextFactorIsSelf() {
+            factor = number - 1
+        }
+    }
+}
--- primefactors/src/test/groovy/kata/PrimeFactorsSpec.groovy
+++ primefactors/src/test/groovy/kata/PrimeFactorsSpec.groovy
@@ -1,6 +1,7 @@
 package kata
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * Prime Factors Kata
@@ -18,6 +19,30 @@ import spock.lang.Specification
  * 2**100 => [2]*100
  * 2**19-1 => [2**19-1]
  * 2**31-1 => [2**31-1]
+ * 131071*(2**19-1) => [131071,2**19-1]
  */
 class PrimeFactorsSpec extends Specification {
+
+    def "returns prime factors of a given number"() {
+        expect:
+        PrimeFactors.of(number) == factors
+
+        where:
+        number                  | factors
+        1                       | []
+        2                       | [2]
+        3                       | [3]
+        4                       | [2, 2]
+        5                       | [5]
+        6                       | [2, 3]
+        7                       | [7]
+        8                       | [2] * 3
+        9                       | [3, 3]
+        2 * 3 * 5 * 7 * 11 * 13 | [2, 3, 5, 7, 11, 13]
+        8191 * 131071           | [8191, 131071]
+        2g ** 100               | [2] * 100
+        2g ** 19 - 1            | [2 ** 19 - 1]
+        2g ** 31 - 1            | [2 ** 31 - 1]
+        131071 * (2g ** 19 - 1) | [131071, 2g ** 19 - 1]
+    }
 }
